c6399d2e12bf013ac49e3caea3ea0de3b0cc76ba
jhycj <jhycj@naver.com>
2022-10-27 15:31:21 +0900

    fix batchsize

--- scripts/train_tsm_imiGUE_rgb_16f.sh
+++ scripts/train_tsm_imiGUE_rgb_16f.sh
@@ -1,5 +1,5 @@
 python ../main.py imiGUE RGB \
      --arch resnet50 --num_segments 16 \
      --gd 20 --lr 0.02 --wd 1e-4 --lr_steps 20 40 --epochs 100 \
-     --batch-size 64 -j 16 --dropout 0.5 --consensus_type=avg --eval-freq=1 \
+     --batch-size 32 -j 16 --dropout 0.5 --consensus_type=avg --eval-freq=1 \
      --shift --shift_div=8 --shift_place=blockres --npb
